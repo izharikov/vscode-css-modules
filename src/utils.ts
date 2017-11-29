807319f5b634e4ed03bf0ff3bb9562d99764e383
Igor <zharikovigor97@gmail.com>
2017-11-29 18:13:56 +0300

    add support work with files without extension specified (works only for import *ss folders)

--- src/utils.ts
+++ src/utils.ts
@@ -3,12 +3,15 @@ import * as path from "path";
 import * as fs from "fs";
 import * as _ from "lodash";
 
+let possibleFileExtension = ['.css', '.scss', '.sass'];
+
 export function getCurrentLine(document: TextDocument, position: Position): string {
     return document.getText(document.lineAt(position).range);
 }
 
 export function genImportRegExp(key: string ): RegExp {
-    const file = "(.+\\.\\S{1,2}ss)";
+    // check file have extension .*ss or folder is css / scss / sass
+    const file = "((.+\\.\\S{1,2}ss)|(.*\\/\\S+ss\\/.*))";
     const fromOrRequire = "(?:from\\s+|=\\s+require(?:<any>)?\\()";
     const requireEndOptional = "\\)?";
     const pattern = `${key}\\s+${fromOrRequire}["']${file}["']${requireEndOptional}`;
@@ -19,7 +22,19 @@ export function findImportPath(text: string, key: string, parentPath: string): s
     const re = genImportRegExp(key);
     const results = re.exec(text);
     if (!!results && results.length > 0) {
-        return path.resolve(parentPath, results[1]);
+        let filePath = path.resolve(parentPath, results[1]);
+        //  check file has extension, matches .css / .sass / .scss
+        if ( filePath.match(/^.*\.+\S{1,2}ss$/g)){
+            return filePath;
+        }
+        //  file have no extension, work with possible extensions
+        for ( let i = 0; i < possibleFileExtension.length; i++){
+            let ext = possibleFileExtension[i];
+            if ( fs.existsSync(filePath + ext)){
+                return filePath + ext;
+            }
+        }
+        return filePath;
     } else {
         return "";
     }
